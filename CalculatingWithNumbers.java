a23543e7febadc305d0d9ea3dce09f1e03b3a4dd
jmeegan2 <jmeegan8@gmail.com>
2022-10-01 17:23:49 -0400

    part 1 variables

---
+++ CalculatingWithNumbers.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class CalculatingWithNumbers {
+    public static void main(String[] args) {
+        //basic math operators + - * /
+
+        //Seconds in day
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("How many days would you like to convert to seconds? ");
+//        int number = Integer.valueOf(scanner.nextLine());
+//        int  ConvertedNum = number * 24 * 60 * 60;
+//        System.out.println(ConvertedNum);
+
+        Scanner scanner = new Scanner(System.in);
+        //sum of numbers
+//        System.out.println("Give first number: ");
+//        int firstNum = Integer.valueOf(scanner.nextLine());
+//        System.out.println("Give second number: ");
+//        int secondNum = Integer.valueOf(scanner.nextLine());
+//        System.out.println("Give third number: ");
+//        int thirdNum = Integer.valueOf(scanner.nextLine());
+//        int sum = firstNum + secondNum + thirdNum;
+//        System.out.println("Sum of nums:  " + sum);
+
+        // Addition formula
+//        System.out.println("Give the first number: ");
+//        int firstNum = Integer.valueOf(scanner.nextLine());
+//        System.out.println("Give the second number: ");
+//        int secondNum = Integer.valueOf(scanner.nextLine());
+//        int sum = firstNum + secondNum;
+//        System.out.println(firstNum + " + " + secondNum + " = " + sum );
+
+        //Multiplication formula
+//        System.out.println("Give the first number: ");
+//        int firstNum = Integer.valueOf(scanner.nextLine());
+//        System.out.println("Give the second number: ");
+//        int secondNum = Integer.valueOf(scanner.nextLine());
+//        int sum = firstNum * secondNum;
+//        System.out.println(firstNum + " * " + secondNum + " = " + sum );
+    }
+}
--- Variables.java
+++ Variables.java
@@ -1,5 +1,69 @@
+import java.util.Scanner;
+
 public class Variables {
-    //Part 4
-    //
+    //Part 1_4
+    public static void main(String[] args) {
+//        int months = 12;
+//        String text = "contains text";
+//        int wholeNumber = 123;
+//        double floatingPoint = 3.141592653;
+//        boolean trueOrFalse = false;
+//
+//        System.out.println("Int: " + wholeNumber);
+//        System.out.println("double: " + floatingPoint);
+//        System.out.println("string: " +  text);
+//        System.out.println("boolean: " + trueOrFalse);
+
+//        int chickenW = 9000;
+//        double baconWkg = 0.1;
+//        String tractorW = "Zero";
+//        String summary = "And finally, a summary: ";
+//
+//        System.out.println("Chicken: ");
+//        System.out.println(chickenW);
+//
+//        System.out.println("Bacon (kg): ");
+//        System.out.println(baconWkg);
+//
+//        System.out.println("Tractor:");
+//        System.out.println(tractorW);
+//
+//        System.out.println("\n" + summary);
+//        System.out.println(chickenW);
+//        System.out.println(baconWkg);
+//        System.out.println(tractorW);
+//
+        //Integer Input
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Give a number: ");
+//        int value = Integer.valueOf(scanner.nextLine());
+//        System.out.println("You gave the number " + value);
+
+        //Double input
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Give a number: ");
+//        double value = Double.valueOf(scanner.nextLine());
+//        System.out.println("You gave the number " + value);
+
+        //Boolean input
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Write something: ");
+//        boolean value = Boolean.valueOf(scanner.nextLine());
+//        System.out.println("True or false? " + value);
+
+        //different types of input
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Give a string: ");
+//        String string = scanner.nextLine();
+//        System.out.println("Give a integer: ");
+//        int integer = Integer.valueOf(scanner.nextLine());
+//        System.out.println("Give a double: ");
+//        double  dbl = Double.valueOf(scanner.nextLine());
+//        System.out.println("Give a boolean: ");
+//        boolean bool = Boolean.valueOf(scanner.nextLine());
+//
+//        System.out.println("Display values string int double and boolean " + string + " " + integer + " "+ dbl +" " + bool );
+
+    }
 }
 
