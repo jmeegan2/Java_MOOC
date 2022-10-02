b57f9c306695f8c8ef9eeb0cf4272620b19e2f77
jmeegan2 <jmeegan8@gmail.com>
2022-10-02 01:21:57 -0400

    just finished part 1 of java programming 1

--- ConditionalStatements.java
+++ ConditionalStatements.java
@@ -71,13 +71,138 @@ public class ConditionalStatements {
 //            System.out.println("The numbers are equal");
 //        }
 
+//    //Grading and Points
+//        System.out.println("Give points [0-100] ");
+//        int grade = Integer.valueOf(scanner.nextLine());
+//        if ( grade < 0 ) {
+//            System.out.println("Impossible!");
+//        } else if ( grade < 50 && grade > 0) {
+//            System.out.println("Failed");
+//        } else if ( grade >= 50 && grade < 60) {
+//            System.out.println("1");
+//        }
+//        else if ( grade >= 60 && grade < 70) {
+//            System.out.println("2");
+//        }
+//        else if ( grade >= 70 && grade < 80) {
+//            System.out.println("3");
+//        }
+//        else if ( grade >= 80 && grade < 90) {
+//            System.out.println("4");
+//        }
+//        else if ( grade >= 90 && grade <= 100) {
+//            System.out.println("5");
+//        }  else if ( grade > 100) {
+//            System.out.println("Incredible");
+//        }
+
+//        for (int grade = -11; grade <= 101; grade++) {
+//            grade = grade + 10;
+//            if ( grade < 0 ) {
+//                System.out.println("Impossible!");
+//            } else if ( grade < 50 && grade > 0) {
+//                System.out.println("Failed");
+//            } else if ( grade >= 50 && grade < 60) {
+//                System.out.println("1");
+//            }
+//            else if ( grade >= 60 && grade < 70) {
+//                System.out.println("2");
+//            }
+//            else if ( grade >= 70 && grade < 80) {
+//                System.out.println("3");
+//            }
+//            else if ( grade >= 80 && grade < 90) {
+//                System.out.println("4");
+//            }
+//            else if ( grade >= 90 && grade <= 100) {
+//                System.out.println("5");
+//            }  else if ( grade > 100) {
+//                System.out.println("Incredible");
+//            }
+//        }
 
 
+
+        //Odd or even
+//        System.out.println("Give a number: ");
+//        int number = Integer.valueOf(scanner.nextLine());
+//        if ( number % 2 == 0) {
+//            System.out.println("The number is even");
+//        } else{
+//            System.out.println("The number is odd");
+//        }
+
+//        for( int number = 0; number <= 100; number++) {
+//            if ( number % 2 == 0) {
+//                System.out.println("The number " + number + " is even");
+//            } else{
+//                System.out.println("The number " + number + " is odd");
+//            }
+//        }
+
+        //Password
+//        System.out.println("Password?");
+//        String password = "Caput Draconis";
+//        String passwordEntered = scanner.nextLine();
+//        if (passwordEntered.equals(password)) {
+//            System.out.println("Welcome");
+//        } else {
+//            System.out.println("Off with you! ");
+//        }
+
+        //Same
+//        System.out.println("Input two strings");
+//        String one = scanner.nextLine();
+//        String two = scanner.nextLine();
+//
+//        if (one.equals(two)) {
+//            System.out.println("Same");
+//        } else {
+//            System.out.println("Different");
+//        }
+
+
+        //Checking the age
+//        System.out.println("How old are you? ");
+//        int age = Integer.valueOf(scanner.nextLine());
+//        if ( age >=0 && age <= 120) {
+//            System.out.println("Ok");
+//        } else {
+//            System.out.println("Impossible!");
+//        }
+
+        //Leap year
+//        System.out.println("Give a year: ");
+//        int year = Integer.valueOf(scanner.nextLine());
+//        if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)) {
+//            System.out.println("This year is a leap year");
+//        } else {
+//            System.out.println("This year is not a leap year");
+//        }
+
+        //Gift tax
+        System.out.println("Value of Gift?");
+        int giftValue = Integer.valueOf(scanner.nextLine());
+        if (giftValue < 5000) {
+            System.out.println("No tax!") ;
+        } else if (giftValue >= 5000 && giftValue < 25000) {
+            double taxOwed = (100 + (giftValue - 5000) * 0.08);
+            System.out.println("Tax: " + taxOwed);
+        } else if (giftValue >= 25000 && giftValue < 55000) {
+            double taxOwed = (1700 + (giftValue - 25000) * 0.10);
+            System.out.println("Tax: " + taxOwed);
+        } else if (giftValue >= 55000 && giftValue < 200000) {
+            double taxOwed = (4700 + (giftValue - 55000) * 0.12);
+            System.out.println("Tax: " + taxOwed);
+        } else if (giftValue >= 200000 && giftValue < 1000000) {
+            double taxOwed = (22100 + (giftValue - 200000) * 0.15);
+            System.out.println("Tax: " + taxOwed);
+        } else if (giftValue >= 1000000) {
+            double taxOwed = (142100 + (giftValue - 100000) * 0.17);
+            System.out.println("Tax: " + taxOwed);
+        }
 
         /*
-
-
-
        Comparison Operators
         > greater than
         >= greater than or equal to
